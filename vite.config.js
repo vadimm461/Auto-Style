26d7e456a41d55d6dda5bf4062f82a9d9c9ccff2
vadimm461 <vadimm461@gmail.com>
2026-07-30 18:47:12 +0300

    Fix Vite base path for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Auto-Style/',
+  base: process.env.GITHUB_ACTIONS ? '/Auto-Style/' : '/',
 });
